749a3d7e0a2b4f0bc794569880025b49b70191b6
NR0617 <94212747+NR0617@users.noreply.github.com>
2022-01-10 18:54:48 +0900

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,29 @@
+#6001
+print('Hello')
+#6002
+print('Hello World')
+#6003
+print('Hello')
+print('World')
+#6004
+print("'Hello'")
+#6005
+print('"Hello World"')
+print("\"Hello World\"")
+#6006
+print("\"!@#$%^&*()'")
+print('"!@#$%^&*()\'')
+#6007
+print('"C:\Download\\\'hello\'.py"')
+print("\"C:\Download\\\'hello\'.py\"")
+#6008
+print('print(\"Hello\\nWorld\")')
+#6009
+print(input())
+c=input()
+print(c)
+#6010
+print(int(input()))
+n=input()
+n=int(n)
+print(n)
